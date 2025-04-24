0a9ecf29544246a1947f23efd5750de841d303f1
anindex <an.thai.le97@gmail.com>
2025-04-24 13:00:23 +0200

    Cached
    Signed-off-by: anindex <an.thai.le97@gmail.com>

--- hydrax/__init__.py
+++ hydrax/__init__.py
@@ -10,3 +10,6 @@ os.environ["XLA_FLAGS"] = "--xla_gpu_triton_gemm_any=true "
 
 # Enable persistent compilation cache
 jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
